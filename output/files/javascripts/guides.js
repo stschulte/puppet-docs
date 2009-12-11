b7fc06a8f1dfbc5fea9807ee3903dda17683f013
Bruce Williams <bruce@codefluency.com>
2009-12-10 17:15:14 -0800

    Add built artifacts to repository.
    Note that this is purely for deployment convenience.  `output/`
    should, under no circumstances, be added to the `master` branch.  For
    new deployments, `git rebase` the `generated` branch on `master`,
    re-generate, add and commit the newly generated documents. Optionally tag.
    * TODO: In the future (when we have a better deployment process set-up),
      this branch should be removed.

---
+++ output/files/javascripts/guides.js
@@ -0,0 +1,7 @@
+function guideMenu(){
+  if (document.getElementById('index-dialog').style.display == "none") {
+    document.getElementById('index-dialog').style.display = "block";
+  } else {
+    document.getElementById('index-dialog').style.display = "none";
+  }
+}
